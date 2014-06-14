d174e116d80a431e4fd2d8c5d90840dc801d347e
Aaron Hull <ahull@cobrain.com>
2014-06-14 11:49:01 -0400

    Initial Commit

---
+++ HCKVOHelper/Classes/HCKVOHelper.h
@@ -0,0 +1,13 @@
+//
+//  HCKVOHelper.h
+//  HCKVOHelper
+//
+//  Created by Aaron Hull on 6/14/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HCKVOHelper : NSObject
+
+@end
---
+++ HCKVOHelper/Classes/HCKVOHelper.m
@@ -0,0 +1,13 @@
+//
+//  HCKVOHelper.m
+//  HCKVOHelper
+//
+//  Created by Aaron Hull on 6/14/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import "HCKVOHelper.h"
+
+@implementation HCKVOHelper
+
+@end
