4ed545a78e77a13b98a4a3d2da904e17c571bfec
Aaron Hull <ahull@cobrain.com>
2014-06-14 11:42:53 -0400

    Initial Commit

---
+++ HCKVOHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCKVOHelper
+//
+//  Created by Aaron Hull on 6/14/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCKVOHelper/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HCKVOHelper
+//
+//  Created by Aaron Hull on 6/14/14.
+//  Copyright (c) 2014 Hull Computing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
